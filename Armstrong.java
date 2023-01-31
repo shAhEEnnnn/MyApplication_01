1a424943e0a3d98cdd50fc3fd581136841a3bda3
MAYUKH GHOSH <93669449+Mack69@users.noreply.github.com>
2023-01-31 15:04:46 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Armstrong
+{
+    public static void main(String args[]) 
+    {
+        Scanner mg = new Scanner(System.in);
+        int num, num1, d,c, sum=0;
+        System.out.println("ENTER THE NUMBER");
+        num= mg.nextInt();
+        num1=num;
+        while(num1>0)
+        {
+            d=num1%10;
+            c=d*d*d;
+            sum=sum+c;
+            num1/=10;
+        }
+        if(num==sum)
+            System.out.println("ARMSTRONG NUMBER");
+        else
+         System.out.println("NOT A ARMSTRONG NUMBER");
+    }
+}
