819f80416fa5e5588a1d250fbb337b10d715e12b
shAhEEnnnn <shaheenamanat810@gmail.com>
2024-12-10 11:33:28 +0000

    10 to 100

--- Armstrong.java
+++ Armstrong.java
@@ -5,18 +5,18 @@ class Armstrong
     {
         Scanner mg = new Scanner(System.in);
         int num, num1, d,c, sum=0;
-        System.out.println("ENTER THE NUMBER");
+        System.out.println("ENTER THE NUMBER=");
         num= mg.nextInt();
         num1=num;
         while(num1>0)
         {
-            d=num1%10;
+            d=num1%100;
             c=d*d*d;
             sum=sum+c;
-            num1/=10;
+            num1/=100;
         }
         if(num==sum)
-            System.out.println("ARMSTRONG NUMBER");
+            System.out.println("ARMSTRONG NUMBER=");
         else
          System.out.println("NOT A ARMSTRONG NUMBER");
     }
