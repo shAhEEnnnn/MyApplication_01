83c8b4f0552231595b0f6c291f84e5cb70304479
shAhEEnnnn <shaheenamanat810@gmail.com>
2024-12-11 15:56:59 +0530

    add phoneno

--- RESUME.java
+++ RESUME.java
@@ -3,12 +3,14 @@ public class RESUME
   public static void main(String args[])
   {
      int age = 20;
-     String name = "haripriyaa";
+     long phoneno=10;
+     String name = "ShaheenParween";
      String dob = "24/03/2002";
      System.out.println("My Personal Details");
      System.out.println("NAME = "+ name);
      System.out.println("DATE OF BIRTH ="+ dob);
      System.out.println("AGE ="+ age);
+     System.out.println("PHONENO ="+ phoneno);
   }
 
 }
