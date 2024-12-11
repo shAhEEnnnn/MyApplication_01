80d7636e88a52f6d259df12ce3543c4ac06d0f98
shAhEEnnnn <shaheenamanat810@gmail.com>
2024-12-11 15:46:27 +0530

    = remove

--- Armstrong.java
+++ Armstrong.java
@@ -5,7 +5,7 @@ class Armstrong
     {
         Scanner mg = new Scanner(System.in);
         int num, num1, d,c, sum=0;
-        System.out.println("ENTER THE NUMBER=");
+        System.out.println("ENTER THE NUMBER");
         num= mg.nextInt();
         num1=num;
         while(num1>0)
@@ -16,7 +16,7 @@ class Armstrong
             num1/=10;
         }
         if(num==sum)
-            System.out.println("ARMSTRONG NUMBER=");
+            System.out.println("ARMSTRONG NUMBER");
         else
      
        System.out.println("NOT A ARMSTRONG NUMBER");
