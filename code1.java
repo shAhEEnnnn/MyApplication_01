fe0d7b3c5c43086ef2288f6517f4c08ba638d122
HariPriya <haripriyaganti@gmail.com>
2024-12-11 05:48:55 +0000

    fswgf

--- code1.java
+++ code1.java
@@ -2,7 +2,7 @@ class code1
 {
     public static void main(String[] args) 
     {
-        int a = 30;
+        int a = 69;
         int b = 20;
         System.out.println("RESULT = " +a*b);
         
